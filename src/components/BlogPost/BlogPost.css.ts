593adf7e9f39abb09cff762cb36cbab1792571fd
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-04-08 17:47:10 -0400

    Actually fix missing markdown styles

--- src/components/BlogPost/BlogPost.css.ts
+++ src/components/BlogPost/BlogPost.css.ts
@@ -1,10 +1,8 @@
-import { globalStyle, style } from "@vanilla-extract/css";
+import { style } from "@vanilla-extract/css";
 
 import { fadeUp } from "../../styles/keyframes.css";
-import { responsiveStyle } from "../../styles/responsiveStyle";
-import { durations, fontSizes, radii, spaces } from "../../styles/scales";
+import { durations } from "../../styles/scales";
 import { sprinkles } from "../../styles/sprinkles.css";
-import { vars } from "../../styles/theme.css";
 
 export const blogPostContainer = style([
   sprinkles({
@@ -56,59 +54,10 @@ export const divider = style([
   { height: "1px" },
 ]);
 
-export const postContent = sprinkles({
-  display: "flex",
-  flexDirection: "column",
-  gap: 24,
-});
-
-globalStyle(`${postContent} p, ${postContent} ul`, {
-  lineHeight: 1.75,
-  fontSize: fontSizes[16],
-});
-
-globalStyle(
-  `${postContent} p, ${postContent} ul`,
-  responsiveStyle({
-    tablet: {
-      fontSize: fontSizes[18],
-    },
+export const postContentContainer = style([
+  sprinkles({
+    display: "flex",
+    flexDirection: "column",
+    gap: 24,
   }),
-);
-
-globalStyle(`${postContent} ul`, {
-  display: "flex",
-  flexDirection: "column",
-  gap: spaces[8],
-  paddingLeft: spaces[24],
-});
-
-globalStyle(`${postContent} li`, {
-  paddingLeft: spaces[4],
-});
-
-globalStyle(`${postContent} pre`, {
-  fontSize: fontSizes[14],
-  padding: spaces[12],
-  borderRadius: radii[2],
-});
-
-globalStyle(
-  `${postContent} pre`,
-  responsiveStyle({
-    tablet: {
-      fontSize: fontSizes[16],
-      padding: spaces[16],
-    },
-  }),
-);
-
-globalStyle(`${postContent} p > code, ${postContent} li > code`, {
-  color: vars.colors.textStrong,
-  backgroundColor: vars.colors.surface3,
-  borderRadius: radii[2],
-  paddingTop: spaces[2],
-  paddingBottom: spaces[2],
-  paddingLeft: spaces[4],
-  paddingRight: spaces[4],
-});
+]);
---
+++ src/components/BlogPost/Markdown.css.ts
@@ -0,0 +1,56 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+import { responsiveStyle } from "../../styles/responsiveStyle";
+import { fontSizes, radii, spaces } from "../../styles/scales";
+import { vars } from "../../styles/theme.css";
+
+globalStyle(`.markdown p, .markdown ul`, {
+  lineHeight: 1.75,
+  fontSize: fontSizes[16],
+});
+
+globalStyle(
+  `.markdown p, .markdown ul`,
+  responsiveStyle({
+    tablet: {
+      fontSize: fontSizes[18],
+    },
+  }),
+);
+
+globalStyle(`.markdown ul`, {
+  display: "flex",
+  flexDirection: "column",
+  gap: spaces[8],
+  paddingLeft: spaces[24],
+});
+
+globalStyle(`.markdown li`, {
+  paddingLeft: spaces[4],
+});
+
+globalStyle(`.markdown pre`, {
+  fontSize: fontSizes[14],
+  padding: spaces[12],
+  borderRadius: radii[2],
+});
+
+globalStyle(
+  `.markdown pre`,
+  responsiveStyle({
+    tablet: {
+      fontSize: fontSizes[16],
+      padding: spaces[16],
+    },
+  }),
+);
+
+globalStyle(`.markdown p > code, .markdown li > code`, {
+  color: vars.colors.textStrong,
+  backgroundColor: vars.colors.surface3,
+  borderRadius: radii[2],
+  paddingTop: spaces[2],
+  paddingBottom: spaces[2],
+  paddingLeft: spaces[4],
+  paddingRight: spaces[4],
+});
