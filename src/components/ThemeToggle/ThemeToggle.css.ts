33b2cdb845fd5789b53051a17fb8e08aa4c17fb3
Blake Zimmerman <blake.zimmerman@icloud.com>
2023-09-26 23:06:09 -0400

    Add theme toggle

---
+++ src/components/ThemeToggle/ThemeToggle.css.ts
@@ -0,0 +1,44 @@
+import { style } from "@vanilla-extract/css";
+
+import { durations } from "../../styles/scales";
+import { sprinkles } from "../../styles/sprinkles.css";
+
+export const themeToggle = style([
+  sprinkles({ size: [20, 24] }),
+  { position: "relative" },
+]);
+
+const themeIcon = style([
+  sprinkles({
+    size: [20, 24],
+  }),
+  {
+    position: "absolute",
+    inset: 0,
+    transition: `opacity ${durations[2]} ease, transform ${durations[2]} ease`,
+  },
+]);
+
+export const sunIcon = style([
+  themeIcon,
+  {
+    selectors: {
+      "button[data-color-scheme='light'] > &": {
+        opacity: 0,
+        transform: "rotate(-90deg)",
+      },
+    },
+  },
+]);
+
+export const moonIcon = style([
+  themeIcon,
+  {
+    selectors: {
+      "button[data-color-scheme='dark'] > &": {
+        opacity: 0,
+        transform: "rotate(90deg)",
+      },
+    },
+  },
+]);
